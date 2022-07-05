1533d9177e57003aeffb87bf1808449bb91844b8
ysmoradi <ysmoradi@outlook.com>
2022-07-06 01:51:21 +0430

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+
+app.MapFallbackToPage("/_Host");
+
+app.Run();
