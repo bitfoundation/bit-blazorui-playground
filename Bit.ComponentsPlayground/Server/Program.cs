c3fa09ce77879a58da9ccd39de77b4676a694061
ysmoradi <ysmoradi@outlook.com>
2022-07-06 10:56:44 +0430

    Switch to blazor web assembly

---
+++ Bit.ComponentsPlayground/Server/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.ResponseCompression;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseHsts();
+}
+
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapRazorPages();
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run();
