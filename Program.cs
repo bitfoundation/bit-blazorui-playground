a37fb0158c62794dc896bc4c1406196743686e49
Yaser Moradi <ysmoradi@outlook.com>
2023-07-10 17:31:22 +0200

    feat: resolve playground run issues #15 (#16)

--- Program.cs
+++ Program.cs
@@ -13,9 +13,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-}
 
-app.UseHttpsRedirection();
+    app.UseHttpsRedirection();
+}
 
 app.UseStaticFiles();
 
