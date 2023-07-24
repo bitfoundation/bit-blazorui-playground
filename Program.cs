4d91ddb10403d27adf9c108916e28b6d522021a3
Yaser Moradi <ysmoradi@outlook.com>
2023-07-24 20:21:50 +0200

    feat: update to 5.2.0-pre-03 #17 (#18)

--- Program.cs
+++ Program.cs
@@ -13,8 +13,12 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+}
 
+if (builder.Environment.IsDevelopment() is false)
+{
     app.UseHttpsRedirection();
+    app.UseResponseCompression();
 }
 
 app.UseStaticFiles();
