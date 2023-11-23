73dfbf2b4b6a17afb08c5cb5a382f2cfb4f1b365
Yaser Moradi <ysmoradi@outlook.com>
2023-11-23 14:12:55 +0100

    update to bit 8.1.0

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+using Bit.BlazorUIPlayground.Components;
+
+var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages();
-builder.Services.AddServerSideBlazor();
+builder.Services.AddRazorComponents()
+                .AddInteractiveServerComponents();
+
 builder.Services.AddBitBlazorUIServices();
 
 var app = builder.Build();
@@ -10,7 +13,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
@@ -22,10 +25,9 @@ if (builder.Environment.IsDevelopment() is false)
 }
 
 app.UseStaticFiles();
+app.UseAntiforgery();
 
-app.UseRouting();
-
-app.MapBlazorHub();
-app.MapFallbackToPage("/_Host");
+app.MapRazorComponents<App>()
+   .AddInteractiveServerRenderMode();
 
 app.Run();
